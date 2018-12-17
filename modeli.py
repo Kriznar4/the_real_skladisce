f901d996cbf12d32fecfdbb673fea89b40578fae
Kriznar4 <karel.kriznar98@gmail.com>
2018-12-17 22:51:12 +0100

    Spremenil, da funkcije poišči vračajo le šifro

--- modeli.py
+++ modeli.py
@@ -51,28 +51,55 @@ def podatki_skladisca():
     """
     return conn.execute(poizvedba).fetchall()
 
-def poisci_izdelek_ime(ime_vnos):
+def poisci_izdelek(ime_vnos):
     """
-    Poišče izdelke le na podlagi imena.
+    Poišče izdelke le na podlagi imena. Vrne seznam šifer.
     """
     poizvedba = """
-        SELECT sifra, ime, velikost_pakiranja, enota, tip_izdelka
+        SELECT sifra
         FROM izdelki
         WHERE ime LIKE ?
     """
-    return conn.execute(poizvedba, ['%' + ime_vnos + '%']).fetchall()
+    return [sifra for sifra, in conn.execute(poizvedba, ['%' + ime_vnos + '%']).fetchall()]
 
-def poisci_partner_ime(ime_vnos):
+def poisci_partner(ime_vnos):
     """
-    Poišče izdelke le na podlagi imena.
+    Poišče izdelke le na podlagi imena. Vrne seznam šifer.
     """
     poizvedba = """
         SELECT sifra
         FROM partnerji
         WHERE ime LIKE ?
     """
-    sifra, = conn.execute(poizvedba, ['%' + ime_vnos + '%']).fetchone()
-    return sifra
+    return [sifra for sifra, in conn.execute(poizvedba, ['%' + ime_vnos + '%']).fetchall()]
+
+def izdelek_podatki(sifra):
+    """
+    Vrne podatke o izdelku z dano šifro.
+
+    [sifra, ime, velikost_pakiranja, enota, kolicina, opis, tip_izdelka, opomnik]
+    """
+    poizvedba = """
+        SELECT sifra, 
+               ime, 
+               velikost_pakiranja, 
+               enota, 
+               kolicina, 
+               opis, 
+               tip_izdelka, 
+               opomnik
+        FROM izdelki
+        WHERE sifra = ?
+    """
+    return conn.execute(poizvedba, [sifra]).fetchone()
+
+def partner_podatki(sifra):
+    """
+    Vrne podatke o partnerju z dano šifro.
+
+[sifra, ddv, ime, naslov, telefon, email]
+    """
+
 
 def v_narocila(partner, komentar = None, datum_narocila = datetime.datetime.now().strftime("%d/%m/%Y")):
     """
@@ -90,5 +117,6 @@ def v_kosarico(st_narocila, sifra_izdelka, cena, popust = 0, kolicina = None, da
     """
     Vstavi v košarico.
     """
+    return
 
-print(poisci_izdelek_ime('li'))
+print(izdelek_podatki(12))
